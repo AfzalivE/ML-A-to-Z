e980bb2d1ea3b752ba212d07a77c2bb62f48c5c7
Afzal Najam <afzalnaj@gmail.com>
2017-11-22 21:36:50 -0500

    Added my PCA, LDA, KPCA

--- Part 9 - Dimensionality Reduction/Section 35 - Linear Discriminant Analysis/lda.R
+++ Part 9 - Dimensionality Reduction/Section 35 - Linear Discriminant Analysis/lda.R
@@ -1,33 +1,39 @@
-# Support Vector Machine (SVM)
+# LDA
 
 # Importing the dataset
-dataset = read.csv('Social_Network_Ads.csv')
-dataset = dataset[3:5]
-
-# Encoding the target feature as factor
-dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+dataset = read.csv('Wine.csv')
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+split = sample.split(dataset$Customer_Segment, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-training_set[-3] = scale(training_set[-3])
-test_set[-3] = scale(test_set[-3])
+training_set[-14] = scale(training_set[-14])
+test_set[-14] = scale(test_set[-14])
 
-# Fitting SVM to the Training set and Predicting the Test set results
+# Applying LDA
+library(MASS)
+lda = lda(formula = Customer_Segment ~ ., data = training_set)
+training_set = as.data.frame(predict(lda, training_set))
+training_set = training_set[c(5, 6, 1)]
+test_set = as.data.frame(predict(lda, test_set))
+test_set = test_set[c(5, 6, 1)]
+
+# Fitting SVM to the Training set
+# install.packages('e1071')
 library(e1071)
-classifier = svm(formula = Purchased ~ .,
+classifier = svm(formula = class ~ .,
                  data = training_set,
                  type = 'C-classification',
                  kernel = 'linear')
 
 # Predicting the Test set results
 y_pred = predict(classifier, newdata = test_set[-3])
+
 # Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred)
 
@@ -37,15 +43,15 @@ set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
+colnames(grid_set) = c('x.LD1', 'x.LD2')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
      main = 'SVM (Training set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
+     xlab = 'LD1', ylab = 'LD2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', ifelse(set[, 3] == 1, 'green4', 'red3')))
 
 # Visualising the Test set results
 library(ElemStatLearn)
@@ -53,11 +59,11 @@ set = test_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
+colnames(grid_set) = c('x.LD1', 'x.LD2')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3], main = 'SVM (Test set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
+     xlab = 'LD1', ylab = 'LD2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', ifelse(set[, 3] == 1, 'green4', 'red3')))
--- Part 9 - Dimensionality Reduction/Section 36 - Kernel PCA/kernel_pca.R
+++ Part 9 - Dimensionality Reduction/Section 36 - Kernel PCA/kernel_pca.R
@@ -1,11 +1,8 @@
-# Support Vector Machine (SVM)
+# Kernel PCA
 
 # Importing the dataset
 dataset = read.csv('Social_Network_Ads.csv')
-dataset = dataset[3:5]
-
-# Encoding the target feature as factor
-dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+dataset = dataset[, 3:5]
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
@@ -16,46 +13,60 @@ training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-training_set[-3] = scale(training_set[-3])
-test_set[-3] = scale(test_set[-3])
+training_set[, 1:2] = scale(training_set[, 1:2])
+test_set[, 1:2] = scale(test_set[, 1:2])
 
-# Fitting SVM to the Training set and Predicting the Test set results
-library(e1071)
-classifier = svm(formula = Purchased ~ .,
-                 data = training_set,
-                 type = 'C-classification',
-                 kernel = 'linear')
+# Applying Kernel PCA
+# install.packages('kernlab')
+library(kernlab)
+kpca = kpca(~., data = training_set[-3], kernel = 'rbfdot', features = 2)
+training_set_pca = as.data.frame(predict(kpca, training_set))
+training_set_pca$Purchased = training_set$Purchased
+test_set_pca = as.data.frame(predict(kpca, test_set))
+test_set_pca$Purchased = test_set$Purchased
+
+# Fitting Logistic Regression to the Training set
+classifier = glm(formula = Purchased ~ .,
+                 family = binomial,
+                 data = training_set_pca)
 
 # Predicting the Test set results
-y_pred = predict(classifier, newdata = test_set[-3])
+prob_pred = predict(classifier, type = 'response', newdata = test_set_pca[-3])
+y_pred = ifelse(prob_pred > 0.5, 1, 0)
+
 # Making the Confusion Matrix
-cm = table(test_set[, 3], y_pred)
+cm = table(test_set_pca[, 3], y_pred)
 
 # Visualising the Training set results
+install.packages('ElemStatLearn')
 library(ElemStatLearn)
-set = training_set
+set = training_set_pca
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set)
+colnames(grid_set) = c('V1', 'V2')
+prob_set = predict(classifier, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
 plot(set[, -3],
-     main = 'SVM (Training set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
+     main = 'Logistic Regression (Training set)',
+     xlab = 'PC1', ylab = 'PC2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
 # Visualising the Test set results
+# install.packages('ElemStatLearn')
 library(ElemStatLearn)
-set = test_set
+set = test_set_pca
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3], main = 'SVM (Test set)',
+colnames(grid_set) = c('V1', 'V2')
+prob_set = predict(classifier, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Logistic Regression (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
